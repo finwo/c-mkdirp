ed4687c726a5a09b420068ad19588df75a0da686
Yersa Nordman <yersa@finwo.nl>
2023-12-07 22:36:21 +0100

    Project init

---
+++ src/mkdirp.c
@@ -0,0 +1,71 @@
+#include <errno.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef _WIN32
+#  include <Windows.h>
+#else
+#  include <sys/stat.h>
+#endif
+
+#include "mkdirp.h"
+
+#ifndef DIRECTORY_SEPARATOR
+#if defined(_WIN32) || defined(_WIN64)
+const char SEP = '\\';
+#else
+#define DIRECTORY_SEPARATOR "/"
+const char SEP = '/';
+#endif
+#endif
+
+bool mkdirp(const char *dirname) {
+    const char *p;
+    char       *temp;
+    bool        ret = true;
+
+    temp = calloc(1, strlen(dirname)+1);
+    /* Skip Windows drive letter. */
+#if defined(_WIN32) || defined(_WIN64)
+    if ((p = strchr(dirname, ':') != NULL) {
+        p++;
+    } else {
+#endif
+        p = dirname;
+#if defined(_WIN32) || defined(_WIN64)
+    }
+#endif
+
+    while ((p = strchr(p, SEP)) != NULL) {
+        /* Skip empty elements. Could be a Windows UNC path or
+           just multiple separators which is okay. */
+        if (p != dirname && *(p-1) == SEP) {
+            p++;
+            continue;
+        }
+        /* Put the path up to this point into a temporary to
+           pass to the make directory function. */
+        memcpy(temp, dirname, p-dirname);
+        temp[p-dirname] = '\0';
+        p++;
+#if defined(_WIN32) || defined(_WIN64)
+        if (CreateDirectory(temp, NULL) == FALSE) {
+            if (GetLastError() != ERROR_ALREADY_EXISTS) {
+                ret = false;
+                break;
+            }
+        }
+#else
+        if (mkdir(temp, 0774) != 0) {
+            if (errno != EEXIST) {
+                ret = false;
+                break;
+            }
+        }
+#endif
+    }
+    free(temp);
+    return ret;
+}
+
---
+++ src/mkdirp.h
@@ -0,0 +1,3 @@
+#include <stdbool.h>
+
+bool mkdirp(const char *dirname);
